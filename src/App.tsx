90f40540e8af9fbc4b4e18fcbe48f3adc93a64c5
caquijano <carlosquijano98@gmail.com>
2021-11-04 15:21:15 -0500

    okey xd

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "bootswatch/dist/lux/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import { Image, Carousel } from "react-bootstrap";
@@ -7,55 +7,76 @@ import Avatar from "./components/images/Avatar.png";
 import android from "./components/images/icons/android.png";
 import backend from "./components/images/icons/backend.png";
 import frontend from "./components/images/icons/frontend.png";
+import ScrollReaveal from "scrollreveal";
+import "./styles.css";
 
 function App() {
   //  const heightWindow = window.screen.height
   //const widthWindow = window.screen.width
+  useEffect(() => {
+    const config = {
+      origin: "down",
+      duration: 6000,
+      delay: 750,
+      distance: "0px",
+      scale: 1,
+      easing: "ease",
+      interval: 100,
+    };
+    ScrollReaveal().reveal(".reveal-1", config);
+  }, []);
+
   return (
-    <>
-      <Navbar />
-      <div style={{ width: "100%" }}>
-        <div>
+    <div style={{ backgroundColor: "#000" }}>
+      <div style={{ width: "100%", backgroundColor: "#000" }}>
+        <div
+          style={{ position: "relative", backgroundColor: "#000", height: 800 }}
+        >
           <Image
             src={Background}
-            style={{ position: "relative" }}
+            style={{ position: "relative", backgroundColor: "#000" }}
             width="100%"
             className="img-fluid"
             alt="Responsive image"
           />
         </div>
-        <div style={{ width: "100%", position: "absolute" }}>
-          <div
-            style={{
-              width: "100%",
-              backgroundColor: "#000",
-              display: "flex",
-              borderTopLeftRadius: "30%",
-              borderTopRightRadius: "30%",
-              marginTop: "-40%",
-              paddingBottom: "50%",
-            }}
-          >
-            <div
-              style={{ width: "100%", textAlign: "center", marginTop: "-10%" }}
-            >
+        <div style={{ width: "100%", position: "absolute", marginTop: -700 }}>
+          <div className="Description">
+            <div style={{ width: "100%", textAlign: "center" }}>
               <Image
+                className="reveal-1"
                 src={Avatar}
                 thumbnail
                 roundedCircle
                 width="25%"
                 style={{ margin: 15 }}
               />
-              <section style={{ padding: "10%", marginTop: "-10%" }}>
-                <h3 style={{ color: "#999", paddingBlock: 20 }}>
-                  Hola, Mi nombre es Carlos Quijano
+              <section style={{ paddingBlock: "5%", paddingInline: "7%" }}>
+                <h3
+                  style={{ color: "#999", paddingBlock: 20 }}
+                  className="reveal-1"
+                >
+                  Hola, Mi nombre es Carlos Quijano{"   "}
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/330/330508.png"
+                    style={{ height: 30 }}
+                  />
                 </h3>
 
-                <h5 style={{ color: "#777" }}>
-                  Soy desarrollador Full-Stack para web y android, a pesar de la
-                  corta experiencia, soy muy apasionado y realmente bueno en lo
-                  que hago, encuentro soluciones faciles, rapidas y usables a
-                  todo tipo de problemas.
+                <h5 style={{ color: "#777" }} className="reveal-1">
+                  Me desempeño como desarrollador Full-Stack tanto para web como
+                  android, a pesar de la corta experiencia, soy muy apasionado y
+                  realmente bueno en lo que hago, encuentro soluciones faciles,
+                  rapidas y usables a todo tipo de problemas, si deseas trabajar
+                  conmigo no dudes en contactarme.
+                </h5>
+                <br />
+                <h5 style={{ color: "#777" }} className="reveal-1">
+                  Cumplí con toda la malla curricular de la carrera profesional
+                  en ingenieria de sistemas, por algunas circuntacias aún no
+                  cuento con mi titulación, pero sigo en aprendizaje continuo, a
+                  demas de codificar, puedo trabajar en cualquier fase del ciclo
+                  de vida del desarrollo de software.
                 </h5>
               </section>
             </div>
@@ -65,20 +86,19 @@ function App() {
               width: "100%",
               backgroundColor: "#e8e8e8",
               display: "flex",
-              marginTop: "-50%",
-              paddingBottom: "50%",
             }}
           >
-            <div
-              style={{ width: "100%", textAlign: "center", marginTop: "-10%" }}
-            >
-              <section style={{ padding: "10%" }}>
-                <h3 style={{ color: "#000", paddingBlock: 20 }}>
+            <div style={{ width: "100%", textAlign: "center" }}>
+              <section style={{ paddingBlock: "4%", paddingInline: "7%" }}>
+                <h3
+                  style={{ color: "#000", paddingBlock: 20 }}
+                  className="reveal-1"
+                >
                   Habilidades principales
                 </h3>
                 <div className="container-fluid content-row">
                   <div className="row">
-                    <div className="col-sm-4 p-3">
+                    <div className="col-sm-4 p-3 reveal-1">
                       <div className="card h-100">
                         <div className="card-body">
                           <Image
@@ -87,8 +107,9 @@ function App() {
                           />
                           <h5>Desarrollador Front-End</h5>
                           <p className="card-text">
-                            With supporting text below as a natural lead-in to
-                            additional content.
+                            Soy apasionado por la codificación y me gusta crear
+                            sitios web atractivos visualmente, intuitivos y
+                            usables.
                           </p>
                           <h5 className="card-title">Lenguajes:</h5>
                           <p className="card-text">
@@ -98,16 +119,14 @@ function App() {
                             Herramientas de desarrollo:
                           </h5>
                           <p className="card-text">Bootstrap</p>
-                          <p className="card-text">CoreUi</p>
                           <p className="card-text">Github</p>
                           <p className="card-text">Gitlab</p>
                           <p className="card-text">Heroku</p>
                           <p className="card-text">Lottie</p>
-                          <p className="card-text">Visual studio</p>
                         </div>
                       </div>
                     </div>
-                    <div className="col-sm-4 p-3">
+                    <div className="col-sm-4 p-3 reveal-1">
                       <div className="card h-100">
                         <div className="card-body">
                           <Image
@@ -116,8 +135,9 @@ function App() {
                           />
                           <h5>Desarrollador Back-End</h5>
                           <p className="card-text">
-                            With supporting text below as a natural lead-in to
-                            additional content.
+                            Desarrollo modulos para la interaccion y el
+                            procesamiento de peticiones que existen entre el
+                            usuario y el sitio web.
                           </p>
                           <h5 className="card-title">Lenguajes:</h5>
                           <p className="card-text">
@@ -133,17 +153,18 @@ function App() {
                         </div>
                       </div>
                     </div>
-                    <div className="col-sm-4 p-3">
+                    <div className="col-sm-4 p-3 reveal-1">
                       <div className="card h-100">
                         <div className="card-body">
                           <Image
                             src={android}
                             style={{ width: 60, paddingBlock: 20 }}
                           />
-                          <h5>Desarrollador Android</h5>
+                          <h5>Desarrollador Movil</h5>
                           <p className="card-text">
-                            With supporting text below as a natural lead-in to
-                            additional content.
+                            Me gusta diseñar, programar y ejecutar sistemas para
+                            marcos mobiles, dando solución a las necesidades de
+                            los usuarios.
                           </p>
                           <h5 className="card-title">Lenguajes:</h5>
                           <p className="card-text">React Native, TypeScript.</p>
@@ -153,7 +174,6 @@ function App() {
                           <p className="card-text">Lottie</p>
                           <p className="card-text">React Native CLI</p>
                           <p className="card-text">React Native Elements</p>
-                          <p className="card-text">Visual studio</p>
                         </div>
                       </div>
                     </div>
@@ -167,16 +187,14 @@ function App() {
               width: "100%",
               backgroundColor: "#fff",
               display: "flex",
-              marginTop: "-50%",
-              paddingBottom: "50%",
             }}
           >
             <section
               style={{
                 width: "100%",
                 textAlign: "center",
-                marginTop: "-10%",
-                padding: "10%",
+                paddingBlock: "4%",
+                paddingInline: "7%",
               }}
             >
               <h3 style={{ color: "#000", paddingBlock: 20 }}>Mis trabajos</h3>
@@ -260,33 +278,9 @@ function App() {
                       <div className="card-body">
                         <h5 className="card-title">SoccerStatistics</h5>
                         <p className="card-text">
-                          Aplicación móvil para obtener las estadisticas deportivas de jugadores del futbol amateur, cuenta con su administracion web  .
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-sm-4 p-3">
-                    <div className="card h-100 ">
-                      <Carousel>
-                        <Carousel.Item>
-                          <img
-                            className="d-block w-100"
-                            src="holder.js/800x400?text=First slide&bg=373940"
-                            alt="First slide"
-                          />
-                          <Carousel.Caption>
-                            <h3>First slide label</h3>
-                            <p>
-                              Nulla vitae elit libero, a pharetra augue mollis
-                              interdum.
-                            </p>
-                          </Carousel.Caption>
-                        </Carousel.Item>  
-                      </Carousel>
-                      <div className="card-body">
-                        <h5 className="card-title">Clon Instagram</h5>
-                        <p className="card-text">
-                          Aplicación móvil para obtener las estadisticas deportivas de jugadores del futbol amateur, cuenta con su administracion web  .
+                          Aplicación móvil para obtener las estadisticas
+                          deportivas de jugadores del futbol amateur, cuenta con
+                          su administracion web .
                         </p>
                       </div>
                     </div>
@@ -310,9 +304,41 @@ function App() {
                         </Carousel.Item>
                       </Carousel>
                       <div className="card-body">
-                        <h5 className="card-title">Administración ferreteria</h5>
+                        <h5 className="card-title">Clon Instagram</h5>
                         <p className="card-text">
-                          Aplicación móvil para obtener las estadisticas deportivas de jugadores del futbol amateur, cuenta con su administracion web  .
+                          Aplicación móvil para obtener las estadisticas
+                          deportivas de jugadores del futbol amateur, cuenta con
+                          su administracion web .
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-sm-4 p-3">
+                    <div className="card h-100 ">
+                      <Carousel>
+                        <Carousel.Item>
+                          <img
+                            className="d-block w-100"
+                            src="holder.js/800x400?text=First slide&bg=373940"
+                            alt="First slide"
+                          />
+                          <Carousel.Caption>
+                            <h3>First slide label</h3>
+                            <p>
+                              Nulla vitae elit libero, a pharetra augue mollis
+                              interdum.
+                            </p>
+                          </Carousel.Caption>
+                        </Carousel.Item>
+                      </Carousel>
+                      <div className="card-body">
+                        <h5 className="card-title">
+                          Administración ferreteria
+                        </h5>
+                        <p className="card-text">
+                          Aplicación móvil para obtener las estadisticas
+                          deportivas de jugadores del futbol amateur, cuenta con
+                          su administracion web .
                         </p>
                       </div>
                     </div>
@@ -338,7 +364,9 @@ function App() {
                       <div className="card-body">
                         <h5 className="card-title">SoccerStatistics</h5>
                         <p className="card-text">
-                          Aplicación móvil para obtener las estadisticas deportivas de jugadores del futbol amateur, cuenta con su administracion web  .
+                          Aplicación móvil para obtener las estadisticas
+                          deportivas de jugadores del futbol amateur, cuenta con
+                          su administracion web .
                         </p>
                       </div>
                     </div>
@@ -347,9 +375,28 @@ function App() {
               </div>
             </section>
           </div>
+          <div
+            style={{
+              width: "100%",
+              backgroundColor: "#000",
+              display: "flex",
+            }}
+          >
+            <section
+              style={{
+                width: "100%",
+                textAlign: "center",
+                paddingTop: "4%",
+                paddingInline: "7%",
+              }}
+            >
+              <h3 style={{ color: "#000", paddingBlock: 20 }}>Mis trabajos</h3>
+              <div className="container-fluid content-row"></div>
+            </section>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavDropdown, Navbar, Container, Nav } from "react-bootstrap";
+import ScrollReaveal from 'scrollreveal'
 
 export default function Navbar2() {
+  useEffect(() => {
+    const config = {
+      origin: 'top',
+      duration: 2000,
+      delay: 150,
+      distance: '70px',
+      scale: 1,
+      easing: 'ease',
+    }
+   ScrollReaveal().reveal(".reveal-navbar", config);
+  }, [])
   return (
-    <Navbar  bg="transparent" expand="lg" style={{position:"fixed", width:"100%", zIndex:5}}>
+    <Navbar className="reveal-navbar"  bg="transparent" expand="lg" style={{position:"fixed", width:"100%", zIndex:5}}>
   <Container>
     <Navbar.Brand > <h4 style={{textShadow:"1px 1px 2px #fff", color:"#000" }}> Carlos Quijano </h4> </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
